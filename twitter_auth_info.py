80666069ec28a77653912556d62289f861a2dfdf
tm-ika <102900238+tm-ika@users.noreply.github.com>
2022-04-05 19:28:44 +0900

    Add files via upload
    認証情報を外部ファイルに分離

---
+++ twitter_auth_info.py
@@ -0,0 +1,5 @@
+# Twitterの認証、取得したキーを格納
+ck = "YOUR_API_KEY"              # consumer_key
+cs = "YOUR_API_SECRET"           # consumer_secret
+at = "YOUR_ACCESS_TOKEN"         # access_token
+ats = "YOUR_ACCESS_TOKEN_SECRET" # access_token_secret
