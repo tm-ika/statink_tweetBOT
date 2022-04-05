b843906fb4957e370d86a3b575449561a23ffd24
tm-ika <102900238+tm-ika@users.noreply.github.com>
2022-04-05 22:53:33 +0900

    Update twitter_auth_info.py

--- twitter_auth_info.py
+++ twitter_auth_info.py
@@ -3,3 +3,5 @@ ck = "YOUR_API_KEY"              # consumer_key
 cs = "YOUR_API_SECRET"           # consumer_secret
 at = "YOUR_ACCESS_TOKEN"         # access_token
 ats = "YOUR_ACCESS_TOKEN_SECRET" # access_token_secret
+
+account_list = [["@YOUR_TWITTER_ACCOUNT","@YOUR_STATINK_ACCOUNT"],["@YOUR_TWITTER_ACCOUNT","@YOUR_STATINK_ACCOUNT"]]
